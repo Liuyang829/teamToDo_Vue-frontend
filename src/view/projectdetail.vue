8179f706385d3718d2cdca13550aee3ad9ccb0f7
wu5040 <wu5040@gmail.com>
2019-02-26 14:50:10 +0800

    项目详情

--- src/view/projectdetail.vue
+++ src/view/projectdetail.vue
@@ -3,23 +3,12 @@
     <Row>
       <Col span="6" offset="8">
         <div class="projecttitle">
-          <p align="center">{{this.$route.params.title}}</p>
+          <p align="center">{{projectInfo.name}}</p>
         </div>
       </Col>
       <Col span="6" offset="4">
         <div align="right">
-          <Button @click="value1 = true" type="primary">Open</Button>
-          <Drawer
-            title="Basic Drawer"
-            :closable="false"
-            v-model="value1"
-            transfer="false"
-            inner="true"
-          >
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-          </Drawer>
+          <Button @click="DrawerValue1 = true" type="primary">Open</Button>
         </div>
       </Col>
     </Row>
@@ -90,6 +79,66 @@
         </ul>
       </Col>
     </Row>
+    <Drawer title="项目详情" v-model="DrawerValue1" width="500" :mask-closable="false" :styles="styles">
+      <Form :model="formItem">
+        <FormItem label="Input">
+          <Input v-model="formItem.input" placeholder="Enter something..."></Input>
+        </FormItem>
+        <FormItem label="Select">
+          <Select v-model="formItem.select">
+            <Option value="beijing">New York</Option>
+            <Option value="shanghai">London</Option>
+            <Option value="shenzhen">Sydney</Option>
+          </Select>
+        </FormItem>
+        <FormItem label="DatePicker">
+          <Row>
+            <Col span="11">
+              <DatePicker type="date" placeholder="Select date" v-model="formItem.date"></DatePicker>
+            </Col>
+            <Col span="2" style="text-align: center">-</Col>
+            <Col span="11">
+              <TimePicker type="time" placeholder="Select time" v-model="formItem.time"></TimePicker>
+            </Col>
+          </Row>
+        </FormItem>
+        <FormItem label="Radio">
+          <RadioGroup v-model="formItem.radio">
+            <Radio label="male">Male</Radio>
+            <Radio label="female">Female</Radio>
+          </RadioGroup>
+        </FormItem>
+        <FormItem label="Checkbox">
+          <CheckboxGroup v-model="formItem.checkbox">
+            <Checkbox label="Eat"></Checkbox>
+            <Checkbox label="Sleep"></Checkbox>
+            <Checkbox label="Run"></Checkbox>
+            <Checkbox label="Movie"></Checkbox>
+          </CheckboxGroup>
+        </FormItem>
+        <FormItem label="Switch">
+          <i-switch v-model="formItem.switch" size="large">
+            <span slot="open">On</span>
+            <span slot="close">Off</span>
+          </i-switch>
+        </FormItem>
+        <FormItem label="Slider">
+          <Slider v-model="formItem.slider" range></Slider>
+        </FormItem>
+        <FormItem label="Text">
+          <Input
+            v-model="formItem.textarea"
+            type="textarea"
+            :autosize="{minRows: 2,maxRows: 5}"
+            placeholder="Enter something..."
+          ></Input>
+        </FormItem>
+        <FormItem>
+          <Button type="primary">Submit</Button>
+          <Button style="margin-left: 8px">Cancel</Button>
+        </FormItem>
+      </Form>
+    </Drawer>
   </div>
 </template>
 
@@ -98,9 +147,30 @@ export default {
   data() {
     // console.log(this.$route.params.title);
     return {
+      projectInfo: "",
       show: true,
-      value1: false,
+      DrawerValue1: false,
+      styles: {
+        height: "calc(100% - 55px)",
+        overflow: "auto",
+        paddingBottom: "53px",
+        position: "static"
+      },
+      formItem: {
+        input: "",
+        select: "",
+        radio: "male",
+        checkbox: [],
+        switch: true,
+        date: "",
+        time: "",
+        slider: [20, 50],
+        textarea: ""
+      }
     };
+  },
+  created: function() {
+    this.projectInfo = this.$route.params.item;
   },
   methods: {}
 };

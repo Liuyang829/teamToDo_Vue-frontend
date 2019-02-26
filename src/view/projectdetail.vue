1aee81c87e58e72c775a4a10cc569f421f8b4700
sunhz0117 <sunhz0117@126.com>
2019-02-26 21:47:57 +0800

    shz2

--- src/view/projectdetail.vue
+++ src/view/projectdetail.vue
@@ -152,13 +152,13 @@
       <div class="model-header" slot="header">
         <p>创建任务</p>
       </div>
-      <Form v-if="createtask" ref="formInline" :model="formInline" :rules="ruleInline">
+      <Form v-if="createtask" ref="taskinfo" :model="taskinfo" :rules="ruleInline">
         <FormItem prop="name">
-          <Input type="text" v-model="formInline.name" placeholder="任务名称（必填）"/>
+          <Input type="text" v-model="taskinfo.name" placeholder="任务名称（必填）" clearable/>
         </FormItem>
 
         <FormItem prop="discription">
-          <Input type="textarea" :rows="4" v-model="formInline.discription" placeholder="任务简介"/>
+          <Input type="textarea" :rows="4" v-model="taskinfo.discription" placeholder="任务简介" clearable/>
         </FormItem>
 
         <FormItem prop="level">
@@ -167,7 +167,7 @@
               style="width:368px"
               :readonly="true"
               type="text"
-              v-model="formInline.level"
+              v-model="taskinfo.level"
               placeholder="级别"
             />
             <div class="status-wrap" slot="content">
@@ -182,7 +182,7 @@
           <DatePicker
             options="start-date-limit"
             type="date"
-            v-model="formInline.start_time"
+            v-model="taskinfo.start_time"
             placeholder="开始时间"
             style="width: 368px"
           ></DatePicker>
@@ -192,30 +192,36 @@
           <DatePicker
             options="finish-date-limit"
             type="date"
-            v-model="formInline.end_time"
+            v-model="taskinfo.end_time"
             placeholder="结束时间"
             style="width: 368px"
           ></DatePicker>
         </FormItem>
 
+        <FormItem label="Select">
+          <Select v-model="taskinfo.member" placeholder="负责人(必选)">
+              <!--Option vue-for="member in projectInfo.member." >{{member.name}}</Option-->
+          </Select>
+        </FormItem>
+
         <FormItem>
-          <Button long type="success" @click="handleSubmit('formInline')">完成并创建</Button>
+          <Button long type="success" @click="handleSubmit('taskinfo')">完成并创建</Button>
         </FormItem>
         
       </Form>
     </Modal>
 
-    <!--Modal :width="400" v-model="changetask" :footer-hide="true">
+    <Modal :width="400" v-model="createtask" :footer-hide="true">
       <div class="model-header" slot="header">
-        <p>任务详情</p>
+        <p>创建任务</p>
       </div>
-      <Form v-if="createtask" ref="formInline" :model="formInline" :rules="ruleInline">
+      <Form v-if="createtask" ref="taskinfo" :model="taskinfo" :rules="ruleInline">
         <FormItem prop="name">
-          <Input type="text" v-model="formInline.name" placeholder="任务名称（必填）"/>
+          <Input type="text" v-model="taskinfo.name" placeholder="任务名称（必填）" clearable/>
         </FormItem>
 
         <FormItem prop="discription">
-          <Input type="textarea" :rows="4" v-model="formInline.discription" placeholder="任务简介"/>
+          <Input type="textarea" :rows="4" v-model="taskinfo.discription" placeholder="任务简介" clearable/>
         </FormItem>
 
         <FormItem prop="level">
@@ -224,7 +230,7 @@
               style="width:368px"
               :readonly="true"
               type="text"
-              v-model="formInline.level"
+              v-model="taskinfo.level"
               placeholder="级别"
             />
             <div class="status-wrap" slot="content">
@@ -237,8 +243,9 @@
 
         <FormItem prop="start_time">
           <DatePicker
+            options="start-date-limit"
             type="date"
-            v-model="formInline.start_time"
+            v-model="taskinfo.start_time"
             placeholder="开始时间"
             style="width: 368px"
           ></DatePicker>
@@ -246,21 +253,28 @@
 
         <FormItem prop="end_time">
           <DatePicker
+            options="finish-date-limit"
             type="date"
-            v-model="formInline.end_time"
+            v-model="taskinfo.end_time"
             placeholder="结束时间"
             style="width: 368px"
           ></DatePicker>
         </FormItem>
 
+        <FormItem label="Select">
+          <Select v-model="taskinfo.member" placeholder="负责人(必选)">
+              <!--Option vue-for="member in projectInfo.member." >{{member.name}}</Option-->
+          </Select>
+        </FormItem>
+
         <FormItem>
-          <Button long type="success" @click="handleSubmit('formInline')">完成并创建</Button>
+          <Button long type="success" @click="handleSubmit('taskinfo')"保存</Button>
         </FormItem>
         
       </Form>
-    </Modal-->
+    </Modal>
 
-    <Drawer title="项目详情" v-model="DrawerValue1" width="500" :mask-closable="false" :styles="styles">
+    <!--Drawer title="项目详情" v-model="DrawerValue1" width="500" :mask-closable="false" :styles="styles">
       <Form :model="formItem">
         <FormItem label="Input">
           <Input v-model="formItem.input" placeholder="Enter something..."></Input>
@@ -319,7 +333,7 @@
           <Button style="margin-left: 8px">Cancel</Button>
         </FormItem>
       </Form>
-    </Drawer>
+    </Drawer-->
   </div>
   
 </template>
@@ -327,93 +341,8 @@
 <script>
 export default {
   data() {
-    // console.log(this.$route.params.title);
     return {
-      //projectid: 
-      createtask: false,
-      changetask: false,
-      poptipShow: false,
-      formInline: {
-        name: "",
-        discription: "",
-        level: "普通",
-        start_time: "",
-        end_time: "",
-        member: ""
-      },
-      ruleInline: {
-        name: [
-          {
-            required: true,
-            message: "请填写任务名称",
-            trigger: "blur"
-          }
-        ],
-      },
-      tasklist: "",
-      show: true,
-      value1: false,
-      // start-date-limit: {
-      //     disabledDate (date) {
-      //         return date && date.valueOf() < Date.now() - 86400000;
-      //     }
-      // },
-      // finish-date-limit: {
-      //     disabledDate (date) {
-      //         return date && date.valueOf() < Date.now() - 86400000;
-      //     }
-      // },
-    };
-  },
-  created: function() {
-    this.axios.get("http://localhost:8090/projects/tasks?project_id={{projectid}}").then((response) => {
-      tasklist=response.data.data;
-      console.log(tasklist);
-    }).catch(function(error) {
-      console.log(error);
-    });
-  },
-  methods: {
-    changeLevel(level) {
-      this.formInline.level = level;
-      this.poptipShow = false;
-    },
-    handleSubmit(name) {  
-      this.$refs[name].validate(valid => {
-        if (valid) {
-          let data = {
-            name: this.formInline.name,
-            description: this.formInline.discription,
-            level: this.formInline.level,
-            state: "待分配",
-            start_time: getFormatDate(new Date(this.formInline.start_time)),
-            end_time: getFormatDate(new Date(this.formInline.end_time)),
-            member: this.formInline.member
-          };
-
-          // this.axios
-          //   .post("http://localhost:8090/projects/", qs.stringify(data))
-          //   .then(res => {
-          //     if (res.data.code == 200) {
-          //       console.log(res);
-          //       this.$Message.success("创建成功");
-          //       this.show = false;
-          //       this.reload();
-          //     } else {
-          //       this.$Message.error("创建失败");
-          //     }
-          //   })
-          //   .catch(res => {
-          //     console.log(res);
-          //   });
-        } else {
-          this.$Message.error("创建失败");
-        }
-      });
-    }
-  },
       projectInfo: "",
-      show: true,
       DrawerValue1: false,
       styles: {
         height: "calc(100% - 55px)",
@@ -431,14 +360,111 @@ export default {
         time: "",
         slider: [20, 50],
         textarea: ""
-      }
+      },
+      createtask: false,
+      changetask: false,
+      poptipShow: false,
+      taskinfo: {
+        name: "",
+        discription: "",
+        level: "普通",
+        start_time: "",
+        end_time: "",
+        member: ""
+      },
+      ruleInline: {
+        name: [
+          {
+            required: true,
+            message: "请填写任务名称",
+            trigger: "blur"
+          }
+        ],
+      },
+      tasklist: "",
+      value1: false,
+      // start-date-limit: {
+      //     disabledDate (date) {
+      //         return date && date.valueOf() < Date.now() - 86400000;
+      //     }
+      // },
+      // finish-date-limit: {
+      //     disabledDate (date) {
+      //         return date && date.valueOf() < Date.now() - 86400000;
+      //     }
+      // },
     };
   },
+  //渲染task
   created: function() {
     this.projectInfo = this.$route.params.item;
+    this.axios.get("http://localhost:8090/projects/tasks?project_id=" + this.projectInfo.id).then((response) => {
+      if(response.data.code==200) {
+        this.tasklist=response.data.data;
+      } else {
+        this.$Message.error("权限错误");
+      }
+    }).catch(function(error) {
+      console.log(error);
+    });
+  },
+  methods: {
+    changeLevel(level) {
+      this.taskinfo.level = level;
+      this.poptipShow = false;
+    },
+    handleSubmit(name) {  
+      // 审核数据
+      this.$refs[name].validate(valid => {
+        if (valid) {
+          // 建数据包
+          let data = {
+            name: this.taskinfo.name,
+            description: this.taskinfo.discription,
+            level: this.taskinfo.level,
+            state: "待分配",
+            start_time: getFormatDate(new Date(this.taskinfo.start_time)),
+            end_time: getFormatDate(new Date(this.taskinfo.end_time)),
+            member: this.taskinfo.member
+          };
+          // POST
+          this.axios
+            .post("http://localhost:8090/projects/", qs.stringify(data))
+            .then(res => {
+              if (res.data.code == 200) {
+                console.log(res);
+                this.$Message.success("任务创建成功");
+                this.createtask = false;
+                this.reload();
+              } else {
+                this.$Message.error("任务创建失败");
+              }
+            })
+            .catch(res => {
+              console.log(res);
+            });
+        } else {
+          this.$Message.error("创建失败");
+        }
+      });
+    }
   },
   methods: {}
 };
+function getFormatDate(date) {
+  var seperator1 = "-";
+  var year = date.getFullYear();
+  var month = date.getMonth() + 1;
+  var strDate = date.getDate() + 1;
+  if (month >= 1 && month <= 9) {
+    month = "0" + month;
+  }
+  if (strDate >= 0 && strDate <= 9) {
+    strDate = "0" + strDate;
+  }
+  var currentdate = year + seperator1 + month + seperator1 + strDate;
+  return currentdate;
+}
 </script>
 
 <style scoped lang="scss">

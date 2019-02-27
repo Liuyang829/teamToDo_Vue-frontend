2f2f8d99fdd4645463183b3c3385502b7ec83dcf
sunhz0117 <sunhz0117@126.com>
2019-02-27 21:25:41 +0800

    shz add and delete

--- src/view/projectdetail.vue
+++ src/view/projectdetail.vue
@@ -8,8 +8,11 @@
       </Col>
       <Col span="6" offset="4">
         <div align="right">
-          <Button @click="DrawerValue1 = true" type="text" ghost>
+          <Button @click="ProjectInfoDraw = true" type="text" ghost>
             <Icon type="md-person-add" size="30" color="black"/>
+          </Button>
+          <Button @click="Message = true" type="text" ghost>
+            <Icon type="ios-chatboxes" size="30" color="black"/>
           </Button>
         </div>
       </Col>
@@ -27,7 +30,7 @@
           <li
             class="task-item"
             v-for="taskitem in tasklist"
-            v-if="taskitem.state==待分配"
+            v-if="taskitem.state == '待分配'"
             :key="taskitem.name"
           >
             <Card long>
@@ -44,11 +47,18 @@
                   </DropdownMenu>
                 </Dropdown>
               </p>
-              <a @click="changetask = true" slot="extra" @click.prevent="changeLimit">
-                <Icon type="ios-loop-strong"></Icon>+ 详细
+              <a slot="extra">
+                <Button @click="deletetask(taskitem.id)" type="text">
+                  <Icon type="ios-trash" size="25"/>
+                </Button>
+                <Button @click="changetask = true" type="text">
+                  <Icon type="ios-clipboard" size="25"/>
+                </Button>
               </a>
               <ul>
-                <li>{{taskitem.description}}</li>
+                <li>负责人：{{taskitem.owner_name}}</li>
+                <Divider class="div"/>
+                <li>任务简介：{{taskitem.description}}</li>
               </ul>
             </Card>
             <br>
@@ -70,8 +80,8 @@
           <li
             class="task-item"
             v-for="taskitem in tasklist"
-            v-if="taskitem.state = 进行中"
-            :key="taskitem.name"
+            v-if="taskitem.state == '进行中'"
+            :key="taskitem.id"
           >
             <Card long>
               <p slot="title">
@@ -87,11 +97,18 @@
                   </DropdownMenu>
                 </Dropdown>
               </p>
-              <a @click="changetask = true" slot="extra" @click.prevent="changeLimit">
-                <Icon type="ios-loop-strong"></Icon>+ 详细
+              <a slot="extra">
+                <Button @click="deletetask(taskitem.id)" type="text">
+                  <Icon type="ios-trash" size="25"/>
+                </Button>
+                <Button @click="changetask = true" type="text">
+                  <Icon type="ios-clipboard" size="25"/>
+                </Button>
               </a>
               <ul>
-                <li>{{taskitem.description}}</li>
+                <li>负责人：{{taskitem.owner_name}}</li>
+                <Divider class="div"/>
+                <li>任务简介：{{taskitem.description}}</li>
               </ul>
             </Card>
             <br>
@@ -109,7 +126,7 @@
           <li
             class="task-item"
             v-for="taskitem in tasklist"
-            v-if="taskitem.state = 已完成"
+            v-if="taskitem.state == '已完成'"
             :key="taskitem.name"
           >
             <Card long>
@@ -126,11 +143,18 @@
                   </DropdownMenu>
                 </Dropdown>
               </p>
-              <a @click="changetask = true" slot="extra">
-                <Icon type="ios-loop-strong"></Icon>+ 详细
+              <a slot="extra">
+                <Button @click="deletetask(taskitem.id)" type="text">
+                  <Icon type="ios-trash" size="25"/>
+                </Button>
+                <Button @click="changetask = true" type="text">
+                  <Icon type="ios-clipboard" size="25"/>
+                </Button>
               </a>
               <ul>
-                <li>{{taskitem.description}}</li>
+                <li>负责人：{{taskitem.owner_name}}</li>
+                <Divider class="div"/>
+                <li>任务简介：{{taskitem.description}}</li>
               </ul>
             </Card>
             <br>
@@ -148,11 +172,11 @@
           <Input type="text" v-model="taskinfo.name" placeholder="任务名称（必填）" clearable/>
         </FormItem>
 
-        <FormItem prop="discription">
+        <FormItem prop="description">
           <Input
             type="textarea"
-            :rows="4"
-            v-model="taskinfo.discription"
+            rows="4"
+            v-model="taskinfo.description"
             placeholder="任务简介"
             clearable
           />
@@ -195,87 +219,19 @@
           ></DatePicker>
         </FormItem>
 
-        <FormItem label="Select">
+        <FormItem prop="owner_id">
           <Select v-model="taskinfo.member" placeholder="负责人(必选)">
-            <!--Option vue-for="member in projectInfo.member." >{{member.name}}</Option-->
+            <Option v-for="memberitem in projectmember" :key="memberitem.id" :value="memberitem.id">{{memberitem.name}}</Option>
           </Select>
         </FormItem>
 
         <FormItem>
-          <Button long type="success" @click="handleSubmit('taskinfo')">完成并创建</Button>
+          <Button long type="success" @click="createsubmit('taskinfo')">完成并创建</Button>
         </FormItem>
       </Form>
     </Modal>
 
-    <Modal :width="400" v-model="createtask" :footer-hide="true">
-      <div class="model-header" slot="header">
-        <p>创建任务</p>
-      </div>
-      <Form v-if="createtask" ref="taskinfo" :model="taskinfo" :rules="ruleInline">
-        <FormItem prop="name">
-          <Input type="text" v-model="taskinfo.name" placeholder="任务名称（必填）" clearable/>
-        </FormItem>
-
-        <FormItem prop="discription">
-          <Input
-            type="textarea"
-            :rows="4"
-            v-model="taskinfo.discription"
-            placeholder="任务简介"
-            clearable
-          />
-        </FormItem>
-
-        <FormItem prop="level">
-          <Poptip v-model="poptipShow" trigger="click" title="级别" content="content">
-            <Input
-              style="width:368px"
-              :readonly="true"
-              type="text"
-              v-model="taskinfo.level"
-              placeholder="级别"
-            />
-            <div class="status-wrap" slot="content">
-              <Button @click="changeLevel('普通')" type="success" class="status-btn" ghost>普通</Button>
-              <Button @click="changeLevel('紧急')" type="warning" class="status-btn" ghost>紧急</Button>
-              <Button @click="changeLevel('非常紧急')" type="error" class="status-btn" ghost>非常紧急</Button>
-            </div>
-          </Poptip>
-        </FormItem>
-
-        <FormItem prop="start_time">
-          <DatePicker
-            options="start-date-limit"
-            type="date"
-            v-model="taskinfo.start_time"
-            placeholder="开始时间"
-            style="width: 368px"
-          ></DatePicker>
-        </FormItem>
-
-        <FormItem prop="end_time">
-          <DatePicker
-            options="finish-date-limit"
-            type="date"
-            v-model="taskinfo.end_time"
-            placeholder="结束时间"
-            style="width: 368px"
-          ></DatePicker>
-        </FormItem>
-
-        <FormItem label="Select">
-          <Select v-model="taskinfo.member" placeholder="负责人(必选)">
-            <!--Option vue-for="member in projectInfo.member." >{{member.name}}</Option-->
-          </Select>
-        </FormItem>
-
-        <FormItem>
-          <Button long type="success" @click="handleSubmit('taskinfo')">保存</Button>
-        </FormItem>
-      </Form>
-    </Modal>
-
-    <Drawer title="项目详情" v-model="DrawerValue1" width="500" :styles="styles" :transfer="false">
+    <Drawer title="项目详情" v-model="ProjectInfoDraw" width="500" :styles="styles" :transfer="false">
       <Form :model="formItem">
         <FormItem label="Input">
           <Input v-model="formItem.input" placeholder="Enter something..."></Input>
@@ -290,7 +246,7 @@
         <FormItem label="DatePicker">
           <Row>
             <Col span="11">
-              <DatePicker type="date" pl务简介aceholder="Select date" v-model="formItem.date"></DatePicker>
+              <DatePicker type="date" placeholder="Select date" v-model="formItem.date"></DatePicker>
             </Col>
             <Col span="2" style="text-align: center">-</Col>
             <Col span="11">
@@ -335,19 +291,83 @@
         </FormItem>
       </Form>
     </Drawer>
+
+    <Drawer title="项目详情" v-model="ProjectInfoDraw" width="500" :styles="styles" :transfer="false">
+      <Form :model="formItem">
+        <FormItem label="Input">
+          <Input v-model="formItem.input" placeholder="Enter something..."></Input>
+        </FormItem>
+        <FormItem label="Select">
+          <Select v-model="formItem.select">
+            <Option value="beijing">New York</Option>
+            <Option value="shanghai">London</Option>
+            <Option value="shenzhen">Sydney</Option>
+          </Select>
+        </FormItem>
+        <FormItem label="DatePicker">
+          <Row>
+            <Col span="11">
+              <DatePicker type="date" placeholder="Select date" v-model="formItem.date"></DatePicker>
+            </Col>
+            <Col span="2" style="text-align: center">-</Col>
+            <Col span="11">
+              <TimePicker type="time" placeholder="Select time" v-model="formItem.time"></TimePicker>
+            </Col>
+          </Row>
+        </FormItem>
+        <FormItem label="Radio">
+          <RadioGroup v-model="formItem.radio">
+            <Radio label="male">Male</Radio>
+            <Radio label="female">Female</Radio>
+          </RadioGroup>
+        </FormItem>
+        <FormItem label="Checkbox">
+          <CheckboxGroup v-model="formItem.checkbox">
+            <Checkbox label="Eat"></Checkbox>
+            <Checkbox label="Sleep"></Checkbox>
+            <Checkbox label="Run"></Checkbox>
+            <Checkbox label="Movie"></Checkbox>
+          </CheckboxGroup>
+        </FormItem>
+        <FormItem label="Switch">
+          <i-switch v-model="formItem.switch" size="large">
+            <span slot="open">On</span>
+            <span slot="close">Off</span>
+          </i-switch>
+        </FormItem>
+        <FormItem label="Slider">
+          <Slider v-model="formItem.slider" range></Slider>
+        </FormItem>
+        <FormItem label="Text">
+          <Input
+            v-model="formItem.textarea"
+            type="textarea"
+            :autosize="{minRows: 2,maxRows: 5}"
+            placeholder="Enter something..."
+          ></Input>
+        </FormItem>
+        <FormItem>
+          <Button type="primary">Submit</Button>
+          <Button style="margin-left: 8px">Cancel</Button>
+        </FormItem>
+      </Form>
+    </Drawer>
+
   </div>
 </template>
 
 <script>
+import qs from "qs";
 export default {
+  inject:['reload'],
   data() {
     return {
-      //projectid:
-
       //wsg
       projectInfo: "",
       projectId: "",
-      DrawerValue1: false,
+      projectmember: "",
+      ProjectInfoDraw: false,
+      Message: false,
       styles: {
         height: "calc(100% - 55px)",
         overflow: "auto",
@@ -372,7 +392,7 @@ export default {
       poptipShow: false,
       taskinfo: {
         name: "",
-        discription: "",
+        description: "",
         level: "普通",
         start_time: "",
         end_time: "",
@@ -406,7 +426,7 @@ export default {
     //获取跳转页面带来的projectId
     this.projectId = this.$route.query.projectId;
     console.log(this.projectId);
-
+    //获取task
     this.axios
       .get("http://localhost:8090/projects/tasks", {
         params: { project_id: this.projectId }
@@ -415,27 +435,18 @@ export default {
         if (response.data.code == 200) {
           this.projectInfo = response.data.data.project;
           this.tasklist = response.data.data.tasks;
-          console.log("aaa", this.projectInfo);
-        }
-        else{
-          this.$Message.error("请求失败");
+          this.projectmember = response.data.data.members;
+          console.log("tasklist:");
+          console.log(this.tasklist);
+          console.log("projectmember:");
+          console.log(this.projectmember);
+        } else {
+          this.$Message.error("权限错误");
         }
       })
       .catch(function(error) {
         console.log(error);
       });
-
-    //shz
-    // this.axios.get("http://localhost:8090/projects/tasks?project_id=" + this.projectId).then((response) => {
-    //   if(response.data.code==200) {
-    //     this.tasklist=response.data.data;
-    //     console.log(this.tasklist);
-    //   } else {
-    //     this.$Message.error("权限错误");
-    //   }
-    // }).catch(function(error) {
-    //   console.log(error);
-    // });
   },
 
   methods: {
@@ -443,41 +454,66 @@ export default {
       this.taskinfo.level = level;
       this.poptipShow = false;
     },
-    handleSubmit(name) {
+    createsubmit(name) {
       // 审核数据
       this.$refs[name].validate(valid => {
         if (valid) {
           // 建数据包
+          
           let data = {
+            project_id: this.projectId,
             name: this.taskinfo.name,
-            description: this.taskinfo.discription,
+            description: this.taskinfo.description,
             level: this.taskinfo.level,
             state: "待分配",
             start_time: getFormatDate(new Date(this.taskinfo.start_time)),
             end_time: getFormatDate(new Date(this.taskinfo.end_time)),
-            member: this.taskinfo.member
+            owner_id: this.taskinfo.member,
           };
+          console.log(data);
           // POST
           this.axios
-            .post("http://localhost:8090/projects/", qs.stringify(data))
+            .post("http://localhost:8090/projects/tasks/", qs.stringify(data))
             .then((response) => {
-              if ((response).data.code == 200) {
-                console.log(res);
+              console.log(response);
+              if (response.data.code == 200) {
+                console.log(111);
                 this.$Message.success("任务创建成功");
                 this.createtask = false;
                 this.reload();
               } else {
                 this.$Message.error("任务创建失败");
               }
-            })
-            .catch(res => {
-              console.log(res);
+            }).catch(error => {
+              console.log(error);
             });
         } else {
           this.$Message.error("创建失败");
         }
       });
-    }
+    },
+
+    deletetask(taskid) {
+      console.log("delete task:",taskid);
+      let data = {
+        project_id: this.projectId,
+        task_id: String(taskid),
+      };
+      console.log(data);
+      this.axios
+        .delete("http://localhost:8090/projects/tasks/", {params:data})
+        .then((response) => {
+          console.log("response");
+          if (response.data.code == 200) {
+            this.$Message.success("任务删除成功");
+            this.reload();
+          } else {
+            this.$Message.error("任务删除失败");
+          }
+        }).catch(error => {
+          console.log(error);
+        });
+    },
   }
 };
 function getFormatDate(date) {
@@ -506,5 +542,8 @@ function getFormatDate(date) {
   font-style: "Hiragino Sans GB";
   color: #464c5b;
   font-size: 20px;
+}
+.div {
+  margin: 10px;
 }
 </style>
